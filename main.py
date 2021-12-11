8b940f6bb93d3612b5e20564e9c305a88501a710
sivokhinK <kirill.sivokhin@yandex.ru>
2021-12-11 11:36:28 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from classes import person
+import matplotlib.pyplot as plt
+
+N = 1000      # number of people
+ROI = 2       # Radius Of Infection
+RECOVERY = 5  # days needed to recover
+
+# populate array of people
+people = [person(0, 0) for _ in range(N)]
+people[0].Infected = True
+
+while True:
+    plot_x = []
+    plot_y = []
+    for i in range(N):
+        people[i].is_infected(people, ROI)
+        if people[i].Infected:
+            people[i].daysInfected += 1
+        people[i].is_recovered(RECOVERY)
+        people[i].change_pos()
+        plot_x.append(people[i].x)
+        plot_y.append(people[i].y)
+        plt.scatter(people[i].x, people[i].y)
+    break
+plt.show()
